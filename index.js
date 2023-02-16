f1ff47636a24a2f9c60b2ca82a684ae77f3ac9d9
Davide Scarsi <davidescarsi93@gmail.com>
2023-02-16 16:30:50 +0100

    inizialized better version

--- index.js
+++ index.js
@@ -1,155 +1,91 @@
-// OGGETTI SELEZIONATI
-let SClose
-let SButton
-let SModul
+// MODALE
 
-const limit = 300 
+const modal = document.getElementById(`modal`)
+const close_button = document.getElementById(`close`)
 
 
 
+const box =  document.getElementById(`box`)
 
-
-const newInput = document.createElement(`input`)
-newInput.type = "text"
-newInput.id = "inputId"
-SBox.appendChild(newInput)
-
-
-const newButton = document.createElement(`button`)
-SBox.appendChild(newButton)
-newButton.id = "buttonId"
-newButton.innerHTML += `CONFERMA`
-
-
-let error = "ERRORE"
-const newError = document.createElement(`h1`)
-newError.id = "ErrorId"
-newError.classList.add(`error`,`all-centered`,`flex-column`)
-newError.innerHTML = `${error}<p>inserisci numeri da 0 a ${limit}</p>` 
+close_button.addEventListener(`click`, () =>{
+	modal.classList.add(`d-none`)
+})
 
 
 
 
+const pokedex = document.getElementById('pokedex');
+//fetching pokemon's name, image, type and id from pokeapi
 
-let MaxPokemons = 0 // da azzerare
 
-newInput.addEventListener("change", (e) => {
-	if(Number(e.target.value)>limit+1){
+
+let arrType = []
+
+
+
+const fetchPokemon = () => {
+    const promises = [];
+    for (let i = 1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        promises.push(fetch(url).then((res) => res.json()));
+		
+    }
+
+    Promise.all(promises).then((results) => {
 
 	
-		return SBox.appendChild(newError)
+        const pokemon = results.map((result) => ({
+            name: result.name,
+            image: result.sprites['front_default'],
+            type: result.types.map((type) => type.type.name).join('_'),
+            id: result.id
+        }));
+		
+        displayPokemon(pokemon);
 
-	}
-	document.getElementById("inputId")
-	MaxPokemons += Number(e.target.value)
-})
-
-
-
-
-
-
-const APISource = `https://pokeapi.co/api/v2`
-
-const pokeContainer = document.getElementById(`cardsContainer`)
-
-
-const galleryURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-vii/icons/`
-
-let pokeName = []
-
-
-
-
-function start() {
-
-	for (let i = 1; i < MaxPokemons + 1; i++) {
-		fetch(`${APISource}/pokemon/${i}`)
-
-			.then((response) => response.json())
-			.then((data) => {
-				pokeName.push(data.species.name)
-
-				pokeContainer.innerHTML += `<div  class="card text-black all-centered" style="width: 18rem;">
-                
-                <img class="card-img-top pokeImg " src="${galleryURL}${i}.png" alt="Card image cap">
-                
-                <div class="card-body">
-                <h5 class="card-title">${data.species.name}</h5>
-                <p id="abilities" class="card-text">Abilità: ${data.abilities[0].ability.name} </p>
-                <p id="abilities" class="card-text">Peso: ${data.weight} </p>
-                <p id="abilities" class="card-text">Altezza: ${data.height} </p>
-                <a href="#" confronto="${data.species.name}" class="btn btn-primary button">IMMAGINE</a>
-                </div>
-                </div>`
+		let info_buttons = [...document.querySelectorAll(`.button`)]			
+		
+		info_buttons.map(el=>{
+			el.addEventListener(`click`, () =>{
+				modal.classList.remove(`d-none`)
 			})
-	}
-}
+		
+		})	
 
 
+    });
+};
+
+// Displaying the pokemon details image, name, type in card
+const displayPokemon = (pokemon) => {
+    const pokemonHTMLString = pokemon
+        .map(
+            (el) => `
+
+		<div id="box" class="col-4 all-centered ">
+			<div class="box all-centered flex-column text-black ${el.type}"> 
+			<h2>${el.name}</h2>
+			<img class="card-image" src="${el.image}"/>
+			<button class="button">INFO</button>
+			</div>
+			
+		</div>
+    `
+        )
+        .join('');
+		
+		
+		
 
 
-function start2 (){
-
-	fetch(`${APISource}/pokemon/?limit=${limit}&offset=0.`)
-
-		.then((response) => response.json())
-		.then((data) => {
+		
 
 
-			SClose = document.querySelectorAll("span.close")
-			SButton = document.querySelectorAll("a.button")
-			SModul = document.getElementById(`modul`)
-
-			for (let i = 0; i < SButton.length; i++) {            // CICLARE I PULSANTI ABILITÁ SELEZIONATI
-				SButton[i].addEventListener(`click`, (e) => {
-					SModul.classList.remove('d-none')
-
-					data.results.map((el, i) => {
-
-						if (el.name === e.target.attributes.confronto.nodeValue) {
-
-							SModul.innerHTML = `<h1 class="display-1 h1-modul position-relative">Pokemon
-                                      <span class="material-symbols-outlined close">
-                                        close
-                                      </span>
-                                    </h1>
-                                    <div class="box-modul all-centered text-black flex-column">
-                                      <h1>${el.name}</h1>
-                                      <img class="card-img-top pokeImgModul " src="${galleryURL}${i + 1}.png" alt="Card image cap">
-                                    </div>`
-						}
-					})
+    pokedex.innerHTML = pokemonHTMLString;
+};
 
 
-					SClose = document.querySelectorAll("span.close")
-
-
-					for (let i = 0; i < SClose.length; i++) {
-						SClose[i].addEventListener(`click`, (e) => {
-							SModul.classList.add('d-none')
-							SModul.innerHTML = ""
-
-						})
-					}
-
-				})
-
-			}
-
-		})
-
-
-}
-
-newButton.addEventListener("click", () => {
-	document.getElementById("ButtonId")
-	start()
-	start2()
-	newInput.classList.add(`d-none`)
-	newButton.classList.add(`d-none`)
-
-})
+fetchPokemon()
 
 
 
